b3f47637de5758c14938e90ff1d2d9fa3ed085d7
Enu Azubike <enuazubike88@gmail.com>
2022-07-15 01:14:11 +0100

    added dbResponseTimeHistogram to monitor db operations

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -5,19 +5,38 @@ import {
   UpdateQuery,
 } from 'mongoose';
 import Product, { ProductDocument } from '../models/product.model';
+import { dbResponseTimeHistogram } from '../utils/metrics';
 
 export async function createProduct(
   input: DocumentDefinition<
     Omit<ProductDocument, 'createdAt' | 'updatedAt' | 'productId'>
   >
 ) {
-  return Product.create(input);
+  const labels = { operation: 'createProduct' };
+  const timer = dbResponseTimeHistogram.startTimer(labels);
+  try {
+    const result = await Product.create(input);
+    timer({ ...labels, success: 'true' });
+    return result;
+  } catch (err: any) {
+    timer({ ...labels, success: 'false' });
+    throw new Error(err.message);
+  }
 }
-export function findProduct(
+export async function findProduct(
   query: FilterQuery<ProductDocument>,
   options: QueryOptions = { lean: true }
 ) {
-  return Product.findOne(query, {}, options);
+  const labels = { operation: 'findProduct' };
+  const timer = dbResponseTimeHistogram.startTimer(labels);
+  try {
+    const result = await Product.findOne(query, {}, options);
+    timer({ ...labels, success: 'true' });
+    return result;
+  } catch (err: any) {
+    timer({ ...labels, success: 'false' });
+    throw new Error(err.message);
+  }
 }
 export function findAndUpdate(
   query: FilterQuery<ProductDocument>,
